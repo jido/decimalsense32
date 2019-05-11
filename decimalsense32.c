4c1f6ff3b4119a1b5eb0d1bb3f468abe4ea92c4f
jido <denisgithub@bredelet.com>
2019-05-11 13:19:56 +0100

    Simplify test loop

--- decimalsense32.c
+++ decimalsense32.c
@@ -403,54 +403,23 @@ int main(int n, char * args[]) {
     printf(" -1.0e-48 / 0.123456e-48 = %.8g\n", numberAsDouble32(div32(asDecimal32(-1.0e-48), 123456)));
     printf(" -3.0e-2 / 0.006e-48 = %.8g\n", numberAsDouble32(div32(asDecimal32(-3.0e-2), 6000)));
     printf(" 9.999999e-48 / 1.0e-54 = %.8g\n", numberAsDouble32(div32(asDecimal32(9.999999e-48), 1)));
-    static double ef[] = { 1e-54, 1e-53, 1e-52, 1e-51, 1e-50,
-        1e-49, 1e-48, 1e-47, 1e-46, 1e-45, 1e-44, 1e-43, 1e-42, 1e-41, 1e-40,
-        1e-39, 1e-38, 1e-37, 1e-36, 1e-35, 1e-34, 1e-33, 1e-32, 1e-31, 1e-30,
-        1e-29, 1e-28, 1e-27, 1e-26, 1e-25, 1e-24, 1e-23, 1e-22, 1e-21, 1e-20,
-        1e-19, 1e-18, 1e-17, 1e-16, 1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10,
-        1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1,
-        1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9,
-        1e10, 1e11, 1e12, 1e13, 1e14, 1e15, 1e16, 1e17, 1e18, 1e19,
-        1e20, 1e21, 1e22, 1e23, 1e24, 1e25, 1e26, 1e27, 1e28, 1e29,    
-        1e30, 1e31, 1e32, 1e33, 1e34, 1e35, 1e36, 1e37, 1e38, 1e39,
-        1e40, 1e41, 1e42, 1e43, 1e44, 1e45, 1e46, 1e47
-    };
-    /*
-    for (int expn = -48; expn <= 47; ++expn)
+    
+    double ee = 1e-48 * 0.0000001;
+    for (int expn = -48; expn <= 47; ++expn, ee *= 10.0)
     {
         for (int units = -9; units <= 9 - 5 * (expn == 47); units += 1 + (expn > -48 && units == -1))
         {
             long r = random();
             long s = r;
-            for (int deci = 0; deci < 10000000; deci += 1 + r & 7)
-            {
-                double f = ((units + deci * 0.00000001) * ef[expn + 54]) + ((double) s * r);
-                if ((random() & 0xff7b6064) == 0x2d586024) printf(" %g ", f);
-                r >>= 3;
-                if (r == 0) r = random();
-            }
-            puts("");
-        }
-    }
-    */
-    
-    printf("3.437519e22 = %.8g = %.8g\n", (3 + 4375190 * 0.0000001) * ef[22 + 54], numberAsDouble32(asDecimal32(3.437519e22)));
-    long s = 90000790;
-    printf("9.000079e-48 = %.8g\n", s * ef[6] * 0.0000001);
-    for (int expn = -48; expn <= 47; ++expn)
-    {
-        for (int units = -9; units <= 9 - 5 * (expn == 47); units += 1 + (expn > -48 && units == -1))
-        {
-            long r = random();
-            //long s = r;
             for (int deci = 0; deci < 10000000; deci += 3 + r % 8)
             {
-                double f = (units * 10000000 + (units >= 0 ? deci : -deci)) * ef[expn + 54] * 0.0000001;
-                decims32 fnum = asDecimal32(f);
-                decims32 num = makeNumber32_(units < 0, abs(units) * 10000000 + deci, expn); //div32(s * r, );, s * r
+                double f = (units * 10000000 + (units >= 0 ? deci : -deci)) * ee;
+                decims32 d = makeNumber32_(units < 0, abs(units) * 10000000 + deci, expn);
+                decims32 num = div32(s * r, d);
                 if ((r & 0xff764) == 0x2d524) {
-                    printf(" %.8g«%c» ", numberAsDouble32(num), 'x' + (num == fnum));
-                    if (num != fnum) {
+                    decims32 fnum = asDecimal32(f);
+                    printf(" %.8g«%c» ", numberAsDouble32(num), 'x' + (d == fnum));
+                    if (d != fnum) {
                         int e;
                         uint32_t x;
                         numberParts32_(fnum, &e, &x);
@@ -463,5 +432,4 @@ int main(int n, char * args[]) {
             puts("");
         }
     }
-    return s;
 }
